e655731642ec21440aba979d83aa790c2fcf3e86
Rocky.Peng <Rocky.Peng@tymphany.com>
2020-10-15 13:23:46 +0800

    add library

---
+++ openlibrary/src/main/java/com/example/openlibrary/ToastUtils.java
@@ -0,0 +1,26 @@
+package com.example.openlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+    private static volatile  ToastUtils mInstance = null;
+    private static Context mContext = null;
+    private ToastUtils(){}
+
+    public static ToastUtils getInstance(Context context){
+        if(mInstance == null){
+            synchronized (ToastUtils.class){
+                if (mInstance == null){
+                    mInstance = new ToastUtils();
+                    mContext = context;
+                }
+            }
+        }
+        return  mInstance;
+    }
+
+    public void showToast(String str){
+        Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
+    }
+}
